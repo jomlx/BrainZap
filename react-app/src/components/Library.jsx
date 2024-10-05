c63f41a7b3f8a5c42521bb6e8c8c74b656e80656
jomlx <mislos.jomel@gmail.com>
2024-10-05 20:38:41 +0800

    Added Hover

--- react-app/src/components/Library.jsx
+++ react-app/src/components/Library.jsx
@@ -5,7 +5,9 @@ function Library() {
         <div className="library-container">
             <div className="library-card">
                 <div className="reviewer-card">
-                    
+                    <h3 className='reviewer-title'> Pomodoro Reviewer</h3>
+                    <p className='sub-title'>Lorem Ipsum is simply dummy
+                        text of the printing and typesetting industry. Lorem Ipsum </p>
                 </div>
             </div>
         </div>
